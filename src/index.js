a9fdc955394270faaf8b9587a0e4bb560f7cb9bd
jonghyeok <jjhsnail@naver.com>
2017-03-16 17:08:13 +0900

    Add hand test

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ var controller  = new leapjs.Controller();
 controller.on('connect', function() {
   console.log("Successfully connected.");
 });
-/*
+
 controller.on('streamingStarted', function() {
   console.log("A Leap device has been connected.");
 });
@@ -16,13 +16,44 @@ controller.on('streamingStarted', function() {
 controller.on('streamingStopped', function() {
   console.log("A Leap device has been disconnected.");
 });
-*/
+
+let outTextStopMsg = false;
+
 controller.on('deviceFrame', function(frame) {
 
+  let outText = "";
+
+  frame.hands.map(function(thisHand){
+
+      if(thisHand.type=='left'){
+        outText += "Left hand ";
+      }
+      if(thisHand.type=='right'){
+        outText += "Right hand ";
+      }
+  });
+
+
+if(outText.length >0){
+  console.log(outText);
+  outTextStopMsg = true;
+}else{
+  if(outTextStopMsg){
+    console.log("Hands out-------------------------------------------");
+    outTextStopMsg=false;
+  }
+
+}
+
+
+
+
+
+/*
 
 console.log(frame.fingers);
 
-
+*/
 
   /*
   var numberOfFingers = frame.fingers.length;
