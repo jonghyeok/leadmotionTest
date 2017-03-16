b032c85b43888a6ac60155bcddb947caa033e79c
jonghyeok <jjhsnail@naver.com>
2017-03-16 16:31:22 +0900

    send soruce

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
-console.log('Yay!');
+import 'babel-polyfill';
+
+const leapjs = require('leapjs');
+
+var controller  = new leapjs.Controller();
+
+
+controller.on('connect', function() {
+  console.log("Successfully connected.");
+});
+/*
+controller.on('streamingStarted', function() {
+  console.log("A Leap device has been connected.");
+});
+
+controller.on('streamingStopped', function() {
+  console.log("A Leap device has been disconnected.");
+});
+*/
+controller.on('deviceFrame', function(frame) {
+
+
+console.log(frame.fingers);
+
+
+
+  /*
+  var numberOfFingers = frame.fingers.length;
+  console.log(numberOfFingers);
+  */
+});
+
+
+
+controller.connect();
