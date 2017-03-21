539a6f07a0ae1fc201e9531541c8b7474b128446
jonghyeok <jjhsnail@naver.com>
2017-03-21 15:04:57 +0900

    change source

--- src/index.js
+++ src/index.js
@@ -1,66 +1,9 @@
-import 'babel-polyfill';
+import leap from 'leapjs';
 
-const leapjs = require('leapjs');
+// leap.loop(() => {});
 
-var controller  = new leapjs.Controller();
+leap.loop(({hands}) => {
+  const fingers = hands.map((elem) => elem.indexFinger.positions[0]);
 
-
-controller.on('connect', function() {
-  console.log("Successfully connected.");
+  console.log(fingers);
 });
-
-controller.on('streamingStarted', function() {
-  console.log("A Leap device has been connected.");
-});
-
-controller.on('streamingStopped', function() {
-  console.log("A Leap device has been disconnected.");
-});
-
-let outTextStopMsg = false;
-
-controller.on('deviceFrame', function(frame) {
-
-  let outText = "";
-
-  frame.hands.map(function(thisHand){
-
-      if(thisHand.type=='left'){
-        outText += "Left hand ";
-      }
-      if(thisHand.type=='right'){
-        outText += "Right hand ";
-      }
-  });
-
-
-if(outText.length >0){
-  console.log(outText);
-  outTextStopMsg = true;
-}else{
-  if(outTextStopMsg){
-    console.log("Hands out-------------------------------------------");
-    outTextStopMsg=false;
-  }
-
-}
-
-
-
-
-
-/*
-
-console.log(frame.fingers);
-
-*/
-
-  /*
-  var numberOfFingers = frame.fingers.length;
-  console.log(numberOfFingers);
-  */
-});
-
-
-
-controller.connect();
